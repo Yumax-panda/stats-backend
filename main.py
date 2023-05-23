2f7ca058661ad69dce94a3de13b6160396214fe4
Yumax-panda <yuma0wall@gmail.com>
2023-05-23 14:48:47 +0900

    added api endpoints

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    status,
+)
 from fastapi.middleware.cors import CORSMiddleware
 
 from deta import Deta
 import dotenv
 import os
 
+
 dotenv.load_dotenv()
 deta = Deta(os.getenv("DB_KEY"))
 app = FastAPI()
+
 
 origins = [
     "http://localhost:3000",
@@ -24,3 +30,66 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/guild/results/{guild_id}")
+async def get_results(guild_id: int):
+    """Get the game results for a guild.
+
+    Parameters
+    ----------
+    guild_id : int
+        The ID of the guild to get the results for.
+
+    Returns
+    -------
+    dict[str, list]
+        The results for the guild.
+
+    Raises
+    ------
+    HTTPException
+        If no results are found for the guild.
+    """
+    db = deta.Base("results")
+    results = db.get(str(guild_id))
+
+    if results is None or not results["data"]:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No results found"
+        )
+    else:
+        return results
+
+
+@app.get("/guild/name/{guild_id}")
+async def get_guild_name(guild_id: int):
+    """Get the name of a guild.
+
+    Parameters
+    ----------
+    guild_id : int
+        The ID of the guild to get the name of.
+
+    Returns
+    -------
+    dict[str, str]
+        The name of the guild. ex: {"name": "Guild Name"}
+
+    Raises
+    ------
+    HTTPException
+        _description_
+    """
+
+    db = deta.Base("guild")
+    name_map = db.get(key="name")
+
+    if (name:=name_map.get(str(guild_id))) is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No guild name found"
+        )
+    else:
+        return {"name": name}
