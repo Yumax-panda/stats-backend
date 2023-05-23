0215efbb81b6a526da995b7bd4fb309e33c29baf
Yumax-panda <yuma0wall@gmail.com>
2023-05-24 02:29:10 +0900

    fixed js

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing import TypedDict, Optional, Literal
 
 from fastapi import (
     FastAPI,
@@ -38,10 +38,12 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 class GamePayload(TypedDict):
+    idx: int
     date: str
     enemy: str
     enemyScore: str
     score: str
+    diff: int
 
 
 class ResultResponse(BaseModel):
@@ -52,40 +54,78 @@ class NameResponse(BaseModel):
     name: str
 
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("api/guild/results/{guild_id}")
-async def get_results(guild_id: int) -> ResultResponse:
-    """Get the game results for a guild.
+@app.get("/api/guild/results/{guild_id}")
+async def get_results(
+    guild_id: int,
+    name: Optional[str] = None,
+    filter: Literal["win", "lose", "all", "draw"] = "all"
+) -> ResultResponse:
+    """Get the results of a guild.
 
     Parameters
     ----------
     guild_id : int
-        The ID of the guild to get the results for.
+        The ID of the guild to get the results of.
+    name : Optional[str], optional
+        The name of the enemy guild to filter by, by default None
+    filter : Literal[&quot;win&quot;, &quot;lose&quot;, &quot;all&quot;, &quot;draw&quot;], optional
+        The filter to apply to the results, by default &quot;all&quot;
 
     Returns
     -------
-    dict[str, list[ResultResponse]
+    ResultResponse
+        The results of the guild. ex: {"data": [{"idx": 0, "date": "2021-01-01", "enemy": "Enemy Guild", "enemyScore": "100", "score": "200", "diff": 100}]}
 
     Raises
     ------
     HTTPException
-        If no results are found for the guild.
+        If no results are found.
     """
 
     db = deta.Base("results")
     results = db.get(str(guild_id))
 
-    if results is None or not results["data"]:
+    if results is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No results found"
         )
     else:
-        return ResultResponse(data=results["data"])
+        new_data = []
+
+        for idx, game in enumerate(results["data"]):
+            diff = int(game["score"]) - int(game["enemyScore"])
+            if filter == "all":
+                pass
+            elif filter == "win":
+                if diff <= 0:
+                    continue
+            elif filter == "lose":
+                if diff >= 0:
+                    continue
+            elif filter == "draw":
+                if diff != 0:
+                    continue
+
+            if name is not None and not name.lower() in game["enemy"].lower():
+                continue
+
+            new_data.append({
+                "idx": idx,
+                "date": game["date"],
+                "enemy": game["enemy"],
+                "enemyScore": game["enemyScore"],
+                "score": game["score"],
+                "diff": diff
+            })
+
+        return ResultResponse(data=new_data)
 
 
 @app.get("api/guild/name/{guild_id}")
@@ -137,17 +177,24 @@ async def guild_details(request: Request, guild_id: int) -> HTMLResponse:
     db = deta.Base("results")
     response = db.get(str(guild_id))
 
-    if response is None or not response["data"]:
+    if response is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="No results found"
         )
     else:
+
+        for idx, data in enumerate(response["data"]):
+            data.update(
+                diff=int(data["score"]) - int(data["enemyScore"]),
+                idx = idx
+            )
+
         return templates.TemplateResponse(
             "guild_details.html",
             context={
                 "request": request,
                 "key": str(guild_id),
-                "data": response["data"]
+                "data": response["data"],
             }
         )
--- static/index.js
+++ static/index.js
@@ -1,2 +1,86 @@
 var nameFilterElement = document.getElementById('teamName');
-var winOrLoseFilterElement = document.getElementById('winOrLose');
+var winOrLoseFilterElement = document.getElementById('winOrLoseOption');
+
+function refreshTable() {
+    console.log("refreshing table");
+    const parts = window.location.pathname.split("/");
+    const guildId = parts[parts.length - 1];
+
+    var currentNameFilter = nameFilterElement.value;
+    var currentWinOrLoseFilter = winOrLoseFilterElement.value;
+
+    var queryParams = [];
+    if (currentNameFilter) {
+        queryParams.push("name=" + currentNameFilter);
+    }
+    if (currentWinOrLoseFilter) {
+        queryParams.push("filter=" + currentWinOrLoseFilter);
+    }
+    var query = queryParams.join("&");
+    var url = `/api/guild/results/${guildId}?${query}`;
+    console.log(url);
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => updateTable(data.data))
+        .catch(error => console.log(error));
+}
+
+function updateTable(data) {
+    console.log(data);
+    var newBody = document.createElement('tbody');
+    data.forEach(result => {
+        var row = document.createElement('tr');
+        var idx = document.createElement('td');
+        var name = document.createElement('td');
+        var date = document.createElement('td');
+        var scores = document.createElement('td');
+        var diff = document.createElement('td');
+
+        idx.innerHTML = result.idx;
+        name.innerHTML = result.enemy;
+        date.innerHTML = result.date;
+        scores.innerHTML = `${result.score} - ${result.enemyScore}`;
+
+        var msg = "Draw";
+        if (result.diff > 0) {
+            msg = `Win (+${result.diff})`;
+            diff.style.color = "red";
+        }
+        else if (result.diff < 0) {
+            msg = `Lose (${result.diff})`;
+            diff.style.color = "skyblue";
+        }
+        diff.innerHTML = msg;
+
+        row.appendChild(idx);
+        row.appendChild(name);
+        row.appendChild(date);
+        row.appendChild(scores);
+        row.appendChild(diff);
+
+        newBody.appendChild(row);
+    });
+    var tBody = document.getElementById('resultTableBody');
+    tBody.parentElement.replaceChild(newBody, tBody);
+    newBody.id = 'resultTableBody';
+
+    document.getElementById('resultTable').classList.remove("d-none");
+}
+
+var nameFilter = undefined;
+function refreshOnNewNameFilter() {
+    var currentNameFilter = nameFilterElement.value;
+    if (currentNameFilter !== nameFilter) {
+        if (nameFilter !== undefined)
+            refreshTable();
+        nameFilter = currentNameFilter;
+    }
+    setTimeout(refreshOnNewNameFilter, 1000);
+}
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    refreshTable();
+    refreshOnNewNameFilter();
+});
