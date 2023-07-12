ef8e6f8e909e9bae758433515d5951ca0b5f9377
Yumax-panda <yuma0wall@gmail.com>
2023-07-12 13:15:18 +0900

    allowed HEAD request type

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET"],
+    allow_methods=["GET", "HEAD"],
     allow_headers=["*"],
 )
 
@@ -52,6 +52,10 @@ class ResultResponse(BaseModel):
 
 @app.get("/")
 async def test() -> TestPayload:
+    return {"message": "test"}
+
+@app.head("/")
+async def test_head() -> TestPayload:
     return {"message": "test"}
 
 
